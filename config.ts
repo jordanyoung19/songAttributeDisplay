79fbe016f4e528a1ec195e243e2064fab425d56d
Jordan Young <jordanby@uci.edu>
2024-02-07 20:44:22 -0800

    adding tsconfig and other config

--- config.ts
+++ config.ts
@@ -6,3 +6,4 @@ export const credentials = {
 };
 
 export const client_id = "6fb0ac615b1c47a5af6c36512a1495ba";
+export const client_secret = "b6c1fe4c31db47d38bf05d2829c7d41e";
--- test.ts
+++ test.ts
@@ -1,7 +1,12 @@
 import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 import { client_id } from "./config";
+import { client_secret } from "./config";
 
-const sdk = SpotifyApi.withClientCredentials(client_id, "secret", ["scope1", "scope2"]);
+console.log(client_secret);
+// const sdk = SpotifyApi.withClientCredentials("client_id", "secret", ["scope1", "scope2"]);
+
+const sdk = SpotifyApi.withUserAuthorization("client-id", "https://localhost:3000", ["scope1", "scope2"]);
+const user = await sdk.currentUser.profile()
 
 const items = await sdk.search("The Beatles", ["artist"]);
 
